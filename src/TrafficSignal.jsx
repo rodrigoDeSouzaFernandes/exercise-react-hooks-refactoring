121051bb745b7589cc92549fd516094d4dd8fbc3
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-03-10 18:34:02 -0300

    exercise-two aprendi

--- src/TrafficSignal.jsx
+++ src/TrafficSignal.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import CarsContext from './context/CarsContext';
 import redSignal from './images/redSignal.jpeg';
 import yellowSignal from './images/yellowSignal.jpeg';
@@ -11,10 +11,9 @@ const renderSignal = (signalColor) => {
   return null;
 };
 
-class TrafficSignal extends Component {
-  render() {
-    const { color } = this.context.signal;
-    const { changeSignal } = this.context;
+function TrafficSignal () {
+    const { color } = useContext(CarsContext)
+    const { changeSignal } = useContext(CarsContext)
     return (
       <div>
         <div className="button-container">
@@ -31,9 +30,7 @@ class TrafficSignal extends Component {
         <img className="signal" src={renderSignal(color)} alt="" />
       </div>
     );
-  }
-};
+  };
 
-TrafficSignal.contextType = CarsContext;
 
 export default TrafficSignal;
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,60 +1,37 @@
 // src/context/Provider.js
 
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import CarsContext from './CarsContext';
 
-class Provider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cars: {
-        red: false,
-        blue: false,
-        yellow: false,
-      },
-      signal: {
-        color: 'red',
-      },
-    }
-    this.moveCar = this.moveCar.bind(this);
-    this.changeSignal = this.changeSignal.bind(this);
-  }
+function Provider (props) {
+  const [color, setColor] = useState('red')
+  const [cars, setCars] = useState({ red: false, blue: false, yellow: false })
 
-  moveCar(car, side) {
-    this.setState({
-      cars: {
-        ...this.state.cars,
-        [car]: side,
-      },
-    });
+  function moveCar(car, side) {
+  setCars({...cars, [car]:side})
   };
 
-  changeSignal(signalColor) {
-    this.setState({
-      signal: {
-        ...this.state.signal,
-        color: signalColor,
-      },
-    });
+  function changeSignal(signalColor) {
+    setColor(signalColor)
   };
 
-  render() {
+  
     const context = {
-      ...this.state,
-      moveCar: this.moveCar,
-      changeSignal: this.changeSignal,
+      color,
+      cars,
+      moveCar,
+      changeSignal,
     };
 
-    const { children } = this.props;
+    const { children } = props;
 
     return (
       <CarsContext.Provider value={context}>
         {children}
       </CarsContext.Provider>
     );
-  }
-};
+  };
 
 Provider.propTypes = {
   children: PropTypes.node.isRequired,
